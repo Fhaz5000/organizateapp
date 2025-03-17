a2d974b6eb0f61c5dc759c958bdecd08dcb7a45c
Fhaz5000 <harvyastaiza@gmail.com>
2025-03-17 17:22:17 -0500

    ACTUALIZACION  TAREAS FORM

--- Frontend/organizate-app/src/app/app.component.ts
+++ Frontend/organizate-app/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { TareaListComponent } from './components/tarea-list/tarea-list.component';
 import { TareaFormComponent } from './components/tarea-form/tarea-form.component';
-
+import { TareaService } from './services/tarea.service';
+import { Tarea, EstadoTarea } from './models/tarea.model';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +12,18 @@ import { TareaFormComponent } from './components/tarea-form/tarea-form.component
 })
 export class AppComponent {
   title = 'Organizate';
+  constructor(private tareaService: TareaService) {}
+
+  crearTarea() {
+    const nuevaTarea: Tarea = {
+      titulo: 'Nueva tarea',
+      descripcion: 'Descripción de la tarea',
+      estado: EstadoTarea.Pendiente // Usar el enum correctamente
+    };
+
+    this.tareaService.agregarTarea(nuevaTarea).subscribe({
+      next: (tareaCreada) => console.log('Tarea creada:', tareaCreada),
+      error: (error) => console.error('Error al crear la tarea:', error)
+    });
+  }
 }
